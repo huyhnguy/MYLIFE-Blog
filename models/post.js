5404ddc16a5760d6abc36cbbc64df022f845e48d
Huy Nguyen <huy.h.nguy@gmail.com>
2024-06-22 12:52:39 -0500

    write models for comments, posts, and users

---
+++ models/post.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    comments: {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    },
+    published: {
+        type: Boolean,
+        required: true,
+    }
+})
+
+const Post = mongoose.model("Post", postSchema);
