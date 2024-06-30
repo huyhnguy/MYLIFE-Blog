1134cf64f1eaa08006f7aa6308692d0f391e8553
Huy Nguyen <huy.h.nguy@gmail.com>
2024-06-29 20:33:27 -0500

    fix bug that clicking routes on the nav bar would duplicate the url params

--- frontend/src/navbar.jsx
+++ frontend/src/navbar.jsx
@@ -2,8 +2,8 @@
 function Navbar() {
     return(
         <ul>
-            <li><a href="./">Home</a></li>
-            <li><a href="./posts">Posts</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/posts">Posts</a></li>
         </ul>
     )
 }
